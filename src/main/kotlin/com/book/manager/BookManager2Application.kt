57977664ff4527cdc7049192ad1103a88682bfac
sakana7392 <k_hiro@kzd.biglobe.ne.jp>
2022-07-26 03:22:55 +0900

    by 6-3

---
+++ src/main/kotlin/com/book/manager/BookManager2Application.kt
@@ -0,0 +1,11 @@
+package com.book.manager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BookManager2Application
+
+fun main(args: Array<String>) {
+    runApplication<BookManager2Application>(*args)
+}
