942a59aec528f66f750b1ea7abe3bcd028af41b8
sakana7392 <k_hiro@kzd.biglobe.ne.jp>
2022-07-26 21:34:09 +0900

    書籍の新規登録を追加

--- src/main/kotlin/com/book/manager/infrastructure/database/repository/BookRepositoryImpl.kt
+++ src/main/kotlin/com/book/manager/infrastructure/database/repository/BookRepositoryImpl.kt
@@ -14,6 +14,7 @@ import com.book.manager.infrastructure.database.mapper.updateByPrimaryKeySelecti
 import com.book.manager.infrastructure.database.record.BookRecord
 import com.book.manager.infrastructure.database.record.custom.BookWithRentalRecord
 import org.springframework.stereotype.Repository
+import com.book.manager.infrastructure.database.mapper.insert
 import java.time.LocalDate
 
 @Suppress("SpringJavaInjectionPointsAutowiringInspection")
@@ -30,9 +31,9 @@ class BookRepositoryImpl(
         return bookWithRentalMapper.selectByPrimaryKey(id)?.let { toModel(it) }
     }
 //
-//    override fun register(book: Book) {
-//        bookMapper.insert(toRecord(book))
-//    }
+    override fun register(book: Book) {
+        bookMapper.insert(toRecord(book))
+    }
 //
 //    override fun update(id: Long, title: String?, author: String?, releaseDate: LocalDate?) {
 //        bookMapper.updateByPrimaryKeySelective(BookRecord(id, title, author, releaseDate))
@@ -60,7 +61,7 @@ class BookRepositoryImpl(
         return BookWithRental(book, rental)
     }
 
-//    private fun toRecord(model: Book): BookRecord {
-//        return BookRecord(model.id, model.title, model.author, model.releaseDate)
-//    }
+    private fun toRecord(model: Book): BookRecord {
+        return BookRecord(model.id, model.title, model.author, model.releaseDate)
+    }
 }
